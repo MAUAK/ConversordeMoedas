1167341904323fe8c83c36522965e14734bab1dc
MAUAK <kauamsilva360@gmail.com>
2021-12-01 21:34:55 -0300

    att

--- ConversordeMoedas/ConversordeMoedas.cs
+++ ConversordeMoedas/ConversordeMoedas.cs
@@ -6,27 +6,30 @@ namespace ConversordeMoedas
 {
     class ConversordeMoedas
     {
-        //Criando a variavel id com encapsulamento
+        //Criando a variavel de quantidade de dólares com encapsulamento
         public double QtdDolares { get; set; }        
-        //Criando a variavel salario com encapsulamento
+        //Criando a variavel de cotação de dólares com encapsulamento
         public double CotacaoDolar { get; set; }
-        //Criando a variavel salario com encapsulamento
+        //Criando a variavel do valor de IOF com encapsulamento
         public double IOF { get; set; }
-        //Criando a variavel salario com encapsulamento
+        //Criando a variavel do valor convertido em reais com encapsulamento
         public double ValorReais { get; set; }
-        //Criando a variavel salario com encapsulamento
+        //Criando a variavel total com encapsulamento
         public double ValorTotal { get; set; }
 
-        //Criando o método para incrementar a porcentagem ao salario
+        //Criando o método para fazer o calculo do valor do dólar em reais
         public void ValorEmReais()
         {
-            //Fazendo o calculo da porcentagem
+            //Fazendo o calculo da conversão
             ValorReais = QtdDolares * CotacaoDolar;
         }
 
+        //Criando o método dpara incrementar a porcentagem do IOF no valor total
         public void ValorTotalIOF()
         {
+            //Fazendo o calculo
             ValorReais += ValorReais * IOF;
+            //Igualando o valor com o IOF na varipavel Valor total
             ValorTotal = ValorReais;
         }        
     }
--- ConversordeMoedas/Program.cs
+++ ConversordeMoedas/Program.cs
@@ -14,26 +14,32 @@ namespace ConversordeMoedas
              * Valor IOF para pessoas fisíicas (4,08%) - https://riconnect.rico.com.vc/blog/iof
              */
 
-            //
+            //Instanciando a classe de Conversor de Moedas
             ConversordeMoedas cm = new ConversordeMoedas();
-
             
-            //
+            //Pedindo qual é a cotação do dólar
             Console.WriteLine("QUAL É A COTAÇÃO DO DÓLAR? ");
+            //Guardando o valor na variável de cotação de dólar
             cm.CotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            //
+            //Pedindo quantos dólares ira ser comprado
             Console.WriteLine("\nQUANTOS DÓLARES VOCÊ IRÁ COMPRAR? ");
+            //Guardando o valor na variável de quantidade de dólares
             cm.QtdDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             
+            //Chamando o método para converter o valor em real
             cm.ValorEmReais();
+            //Imprimindo o valor de dólares em real
             Console.WriteLine("\nVALOR A SER PAGO EM REAIS = " + cm.ValorReais.ToString("F2", CultureInfo.InvariantCulture));
             
-            //
+            //Pedindo qual irá seo o valor do IOF
             Console.WriteLine("\nVALOR A SER PAGO DE IOF: ");
+            //Guardando o valor do IOF na variável IOF
             cm.IOF = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            //Chamando o método para fazer o valor total com o IOF
             cm.ValorTotalIOF();
+            //Imprimindo o valor total
             Console.WriteLine("\nVALOR TOTAL DA COMPRA COM IOF = " + cm.ValorTotal);
 
         }
