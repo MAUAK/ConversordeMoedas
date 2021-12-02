a8144e646dc4ca229b32a84061fa47b5a94e1d8f
MAUAK <kauamsilva360@gmail.com>
2021-12-01 21:24:07 -0300

    Adicionar arquivos de projeto.

---
+++ ConversordeMoedas/ConversordeMoedas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversordeMoedas
+{
+    class ConversordeMoedas
+    {
+        //Criando a variavel id com encapsulamento
+        public double QtdDolares { get; set; }        
+        //Criando a variavel salario com encapsulamento
+        public double CotacaoDolar { get; set; }
+        //Criando a variavel salario com encapsulamento
+        public double IOF { get; set; }
+        //Criando a variavel salario com encapsulamento
+        public double ValorReais { get; set; }
+        //Criando a variavel salario com encapsulamento
+        public double ValorTotal { get; set; }
+
+        //Criando o método para incrementar a porcentagem ao salario
+        public void ValorEmReais()
+        {
+            //Fazendo o calculo da porcentagem
+            ValorReais = QtdDolares * CotacaoDolar;
+        }
+
+        public void ValorTotalIOF()
+        {
+            ValorReais += ValorReais * IOF;
+            ValorTotal = ValorReais;
+        }        
+    }
+}
---
+++ ConversordeMoedas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversordeMoedas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * ----------------- KAUAM SILVA BATISTA - 210501 ----------- 
+             * 
+             * Valor do dólar (R$5,59) - https://economia.uol.com.br/noticias/redacao/2021/11/24/dolar-comercial-opera-em-queda-r-5606.htm
+             * Valor IOF para pessoas fisíicas (4,08%) - https://riconnect.rico.com.vc/blog/iof
+             */
+
+            //
+            ConversordeMoedas cm = new ConversordeMoedas();
+
+            
+            //
+            Console.WriteLine("QUAL É A COTAÇÃO DO DÓLAR? ");
+            cm.CotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //
+            Console.WriteLine("\nQUANTOS DÓLARES VOCÊ IRÁ COMPRAR? ");
+            cm.QtdDolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            cm.ValorEmReais();
+            Console.WriteLine("\nVALOR A SER PAGO EM REAIS = " + cm.ValorReais.ToString("F2", CultureInfo.InvariantCulture));
+            
+            //
+            Console.WriteLine("\nVALOR A SER PAGO DE IOF: ");
+            cm.IOF = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            cm.ValorTotalIOF();
+            Console.WriteLine("\nVALOR TOTAL DA COMPRA COM IOF = " + cm.ValorTotal);
+
+        }
+    }
+}
